fccdbe117e65280ddff9cb0b26e0e98602328c92
lightrao <lightrao@hotmail.com>
2024-12-08 19:08:40 +0800

    Named Routes Challenge

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flash_chat_flutter_24/screens/chat_screen.dart';
+import 'package:flash_chat_flutter_24/screens/login_screen.dart';
+import 'package:flash_chat_flutter_24/screens/registration_screen.dart';
+import 'package:flash_chat_flutter_24/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
+    );
+  }
+}
